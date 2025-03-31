a63d6ec6c5726abf770d24a093720afaf8e83b02
JChampi <70489487@continental.edu.pe>
2025-03-30 20:53:30 -0500

    Versio 1.0

---
+++ inpar.py
@@ -0,0 +1,7 @@
+numero = int(input("Ingresa un numero: "))
+
+# Verificar si el número es par o impar
+if numero % 2 == 0:
+    print(f"El numero {numero} es par.")
+else:
+    print(f"El numero {numero} es impar.")
